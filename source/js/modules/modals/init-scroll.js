29c434aa500c93aadace6cbd21d3a912a21d50ca
Denis Makushin <the.makushin@yandex.ru>
2022-07-17 20:40:42 +0300

    Внесение изменений по критериям, плюс проверка lint, editorconfig, validator, yoxel

--- source/js/modules/modals/init-scroll.js
+++ source/js/modules/modals/init-scroll.js
@@ -1,13 +1,13 @@
 // import smoothscroll from 'smoothscroll-polyfill';
 // smoothscroll.polyfill();
-export default function initScroll(){
+export default function initScroll() {
   const navLink = document.querySelectorAll('.page-header__link');
 
   navLink.forEach((element) => {
-  element.addEventListener('click', (e) => {
-  e.preventDefault();
-  const elementId = e.currentTarget.attributes.href.nodeValue;
-  document.querySelector(elementId).scrollIntoView({ behavior: 'smooth' });
-  })
-})
+    element.addEventListener('click', (e) => {
+      e.preventDefault();
+      const elementId = e.currentTarget.attributes.href.nodeValue;
+      document.querySelector(elementId).scrollIntoView({ behavior: 'smooth' });
+    });
+  });
 }

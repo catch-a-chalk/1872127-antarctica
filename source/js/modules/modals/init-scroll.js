2abdc31da6c474b70558f55c0a15c56260005b04
Denis Makushin <the.makushin@yandex.ru>
2022-07-18 20:31:37 +0300

    Финальные правки перед код-ревью

--- source/js/modules/modals/init-scroll.js
+++ source/js/modules/modals/init-scroll.js
@@ -1,13 +1,14 @@
-// import smoothscroll from 'smoothscroll-polyfill';
-// smoothscroll.polyfill();
+import {toggleNav} from './init-nav';
+
 export default function initScroll() {
   const navLink = document.querySelectorAll('.page-header__link');
 
   navLink.forEach((element) => {
     element.addEventListener('click', (e) => {
       e.preventDefault();
+      toggleNav(false);
       const elementId = e.currentTarget.attributes.href.nodeValue;
-      document.querySelector(elementId).scrollIntoView({ behavior: 'smooth' });
+      document.querySelector(elementId).scrollIntoView({behavior: 'smooth'});
     });
   });
 }
